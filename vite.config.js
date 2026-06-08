3d88d2c5a83c73f732ef56fac221f061dad86329
diggajbishnoi-dot <diggajbishnoi@gmial.com>
2026-06-08 17:46:48 +0530

    Fixed Github Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Library-Management-System/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
