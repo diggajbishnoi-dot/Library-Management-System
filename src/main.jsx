9c85ca28d71d3250ed47046cbbeba20bf39aa277
diggajbishnoi-dot <diggajbishnoi@gmial.com>
2026-06-08 19:01:50 +0530

    teating

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Catalog from './pages/Catalog.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Catalog/>
   </StrictMode>,
 )
