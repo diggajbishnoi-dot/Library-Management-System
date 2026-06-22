ecf42e249784b32a1604a97add8a77c9330f7c4c
diggajbishnoi-dot <diggajbishnoi@gmial.com>
2026-06-22 13:50:33 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import Catalog from './pages/Catalog.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Catalog/>
+    
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/Library-Management-System/', // Yahan repo ka naam aayega
   plugins: [react()]
 })
