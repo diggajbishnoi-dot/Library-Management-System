f4e27f84fb04beb0501175dfda67de1c7ed3b22c
diggajbishnoi-dot <diggajbishnoi@gmial.com>
2026-06-22 14:13:20 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import App from './App.jsx'
 import Catalog from './pages/Catalog.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
     <App />
-    
-  </StrictMode>,
+     
 )
